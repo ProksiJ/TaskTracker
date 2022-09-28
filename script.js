ad7c2158857e8060aa59307e2c91857b9ee28ff4
Ксения Томашева <k.tomasheva@dreamsoft.by>
2022-09-28 17:47:52 +0300

    temp

---
+++ script.js
@@ -0,0 +1,108 @@
+"use strict";
+const currentTasks = document.getElementById("currentTasks");
+const completedTasks = document.getElementById("completedTasks");
+
+const inputText = document.getElementById("inputText");
+const inputTitle = document.getElementById("inputTitle");
+
+const sortButtons = document.getElementById("sortButtons");
+
+const addButton = document.getElementById("addButton");
+const addTaskForm = document.getElementById("addTaskForm");
+const array = [];
+const data = { title: "title", text: "text", priority: "low" };
+currentTasks.insertAdjacentHTML("beforeend", taskTemplate(data));
+
+addTaskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(addTaskForm);
+  const data = Object.fromEntries(formData);
+  array.push(data);
+  localStorage.setItem("currentTasks", JSON.stringify(array));
+  currentTasks.insertAdjacentHTML("beforeend", taskTemplate(data));
+  addTaskForm.reset();
+  $("#exampleModal").modal("hide");
+});
+
+currentTasks.addEventListener("click", (e) => {
+  let task = e.target.closest("li");
+  if (e.target.className === "btn btn-danger w-100") {
+    task.remove();
+  }
+  if (e.target.className === "btn btn-success w-100") {
+    task.remove();
+    completedTasks.insertAdjacentElement("beforeend", task);
+  }
+  if (e.target.className === "btn btn-info w-100 my-2") {
+    $("#exampleModal").modal("show");
+    inputTitle.value = task.getElementsByTagName("h5")[0].innerText;
+    inputText.value = task.getElementsByTagName("p")[0].innerText;
+  }
+});
+
+completedTasks.addEventListener("click", (e) => {
+  if (e.target.className === "btn btn-danger w-100") {
+    let task = e.target.closest("li");
+    task.remove();
+    console.log(task);
+  }
+  if (e.target.className === "btn btn-success w-100") {
+    let task = e.target.closest("li");
+    task.remove();
+    completedTasks.insertAdjacentElement("beforeend", task);
+  }
+});
+
+function getDate() {
+  const date = new Date();
+  const dd = date.getDate() > 9 ? date.getDate() : "0" + date.getDate();
+  const mm =
+    date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1);
+  const hours = date.getHours() > 9 ? date.getHours() : "0" + date.getHours();
+  const min =
+    date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+  return hours + ":" + min + " " + dd + "." + mm + "." + date.getFullYear();
+}
+
+function taskTemplate(data) {
+  return `<li class="list-group-item d-flex w-100 mb-2">
+    <div class="w-100 mr-2">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${data.title}</h5>
+        <div>
+          <small class="mr-2">${data.priority} priority</small>
+          <small>${getDate()}</small>
+        </div>
+      </div>
+      <p class="mb-1 w-100">
+        ${data.text}
+      </p>
+    </div>
+    <div class="dropdown m-2 dropleft">
+      <button
+        class="btn btn-secondary h-100"
+        type="button"
+        id="dropdownMenuItem1"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        <i class="fas fa-ellipsis-v"></i>
+      </button>
+      <div
+        class="dropdown-menu p-2 flex-column"
+        aria-labelledby="dropdownMenuItem1"
+      >
+        <button type="button" class="btn btn-success w-100">
+          Complete
+        </button>
+        <button type="button" class="btn btn-info w-100 my-2">
+          Edit
+        </button>
+        <button type="button" class="btn btn-danger w-100">
+          Delete
+        </button>
+      </div>
+    </div>
+    </li>`;
+}
