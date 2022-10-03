baa039ac415c56846454aca91652c9990812a7d8
ProksiJ <tomasheva.ksenia@yandex.ru>
2022-10-03 13:28:37 +0300

    add task, delete task

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 "use strict";
 const currentTasks = document.getElementById("currentTasks");
 const completedTasks = document.getElementById("completedTasks");
-
 const inputText = document.getElementById("inputText");
 const inputTitle = document.getElementById("inputTitle");
-
 const sortButtons = document.getElementById("sortButtons");
-
-const addButton = document.getElementById("addButton");
 const addTaskForm = document.getElementById("addTaskForm");
-const array = [];
-const data = { title: "title", text: "text", priority: "low" };
-currentTasks.insertAdjacentHTML("beforeend", taskTemplate(data));
+const titleToDo = document.getElementById('toDo')
+
+let currentTasksArray, completedTasksArray
+window.addEventListener('DOMContentLoaded', (event) => {
+ currentTasksArray = localStorage.getItem("currentTasks")
+  ? JSON.parse(localStorage.getItem("currentTasks"))
+  : [];
+
+ completedTasksArray = localStorage.getItem("completedTasks")
+  ? JSON.parse(localStorage.getItem("completedTasks"))
+  : [];
+console.log(currentTasksArray);
+
+if (currentTasksArray.length > 0) {
+  /* titleToDo.innerText = titleToDo.innerText + ` (${currentTasksArray.length})` */
+  currentTasksArray.map((task) => {
+    currentTasks.insertAdjacentHTML("beforeend", taskTemplate(task));
+  });
+}
+
+if (completedTasksArray) {
+  completedTasksArray.map((task) => {
+    completedTasks.insertAdjacentHTML("beforeend", taskTemplate(task));
+  });
+}
+});
+
 
 addTaskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(addTaskForm);
   const data = Object.fromEntries(formData);
-  array.push(data);
-  localStorage.setItem("currentTasks", JSON.stringify(array));
+  data.id = Date.now();
+  currentTasksArray.push(data);
+  localStorage.setItem("currentTasks", JSON.stringify(currentTasksArray));
   currentTasks.insertAdjacentHTML("beforeend", taskTemplate(data));
   addTaskForm.reset();
   $("#exampleModal").modal("hide");
@@ -28,28 +49,39 @@ currentTasks.addEventListener("click", (e) => {
   let task = e.target.closest("li");
   if (e.target.className === "btn btn-danger w-100") {
     task.remove();
+    currentTasksArray = currentTasksArray.filter(
+      (element) => element.id != task.id
+    );
+    localStorage.setItem("currentTasks", JSON.stringify(currentTasksArray));
   }
   if (e.target.className === "btn btn-success w-100") {
     task.remove();
+    completedTasksArray.push(currentTasksArray.find(element => element.id == task.id));
+    currentTasksArray = currentTasksArray.filter(
+      (element) => element.id != task.id
+    );
+    localStorage.setItem("currentTasks", JSON.stringify(currentTasksArray));
+    localStorage.setItem("completedTasks", JSON.stringify(completedTasksArray));
     completedTasks.insertAdjacentElement("beforeend", task);
   }
   if (e.target.className === "btn btn-info w-100 my-2") {
-    $("#exampleModal").modal("show");
+     $("#exampleModal").modal("show");
     inputTitle.value = task.getElementsByTagName("h5")[0].innerText;
     inputText.value = task.getElementsByTagName("p")[0].innerText;
   }
 });
 
 completedTasks.addEventListener("click", (e) => {
+  let task = e.target.closest("li");
   if (e.target.className === "btn btn-danger w-100") {
-    let task = e.target.closest("li");
     task.remove();
-    console.log(task);
+    completedTasksArray = completedTasksArray.filter(
+      (element) => element.id != task.id
+    );
+    localStorage.setItem("currentTasks", JSON.stringify(completedTasksArray));
   }
   if (e.target.className === "btn btn-success w-100") {
-    let task = e.target.closest("li");
-    task.remove();
-    completedTasks.insertAdjacentElement("beforeend", task);
+    
   }
 });
 
@@ -65,7 +97,7 @@ function getDate() {
 }
 
 function taskTemplate(data) {
-  return `<li class="list-group-item d-flex w-100 mb-2">
+  return `<li class="list-group-item d-flex w-100 mb-2" id=${data.id}>
     <div class="w-100 mr-2">
       <div class="d-flex w-100 justify-content-between">
         <h5 class="mb-1">${data.title}</h5>
